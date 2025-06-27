8b3b23acf660aa34ac89bfacaacd6708355146c9
Yiwen Wang <wangyiwenau@gmail.com>
2025-06-27 15:24:17 +1000

    display user info on frontpage

--- src/DisplayData.tsx
+++ src/DisplayData.tsx
@@ -1,5 +1,27 @@
 import { gql, useQuery } from "@apollo/client";
-
+enum Nationality {
+  CANADA,
+  BRAZIL,
+  INDIA,
+  GERMANY,
+  CHILE,
+  UKRAINE,
+}
+export type User = {
+  id: string;
+  name: string;
+  username: string;
+  age: number;
+  nationality: Nationality;
+  friends?: [User];
+  favoriteMovies?: [Movie];
+};
+export type Movie = {
+  id: string;
+  name: string;
+  yearOfPublication: number;
+  isInTheaters: boolean;
+};
 const DisplayData = () => {
   const QUERY_ALL_USERS = gql`
     query GetAllUsers {
@@ -8,12 +30,25 @@ const DisplayData = () => {
         name
         username
         age
+        nationality
       }
     }
   `;
-  const { data } = useQuery(QUERY_ALL_USERS);
+  const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+  if (loading) return <h1>Data is Loading</h1>;
   if (data) console.log(data);
-  return <div>data</div>;
+  if (error) console.error(error);
+  if (data.users) {
+    return data.users.map((user: User) => {
+      return (
+        <>
+          <div>{user.name}</div>
+          <div>{user.age}</div>
+          <div>{user.nationality}</div>
+        </>
+      );
+    });
+  }
 };
 
 export default DisplayData;
